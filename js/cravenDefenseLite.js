ae626ae7ab1d52e6da4ce549193000e424287ad1
Jörgen Thelin <info@craven-studio.com>
2016-07-04 21:22:25 +0200

    Killing enemies
    Still bullets not working

--- js/cravenDefenseLite.js
+++ js/cravenDefenseLite.js
@@ -3,7 +3,6 @@
 var canvas = document.getElementById('cravenDefense');
 var ctx = canvas.getContext('2d');
 var raf;
-var now = Date.now();
 var player = {
     lives: 10,
     money: 100,
@@ -16,9 +15,11 @@ var turret = function(x,y,radius) {
     this.radius = radius,
     this.color = 'blue',
     this.lives = 10,
-    this.active = true,
-    this.range = 20,
-    this.damage = 20,
+    this.engaged = false,
+    this.range = 50,
+    this.damage = 10,
+    this.cost = 100,
+    this.bulletSpeed = 2,    
     this.draw = function() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
@@ -35,7 +36,8 @@ var monster = function(x,y,velocity,radius) {
     this.vy = velocity.y,
     this.radius = radius,
     this.color = 'red',
-    this.lives = 10,
+    this.lives = 100,
+    this.cost = 10,
     this.active = true,
     this.draw = function() {
         ctx.beginPath();
@@ -47,24 +49,22 @@ var monster = function(x,y,velocity,radius) {
 };
 
 /******* Monsters *********/
-var monsters = [], velocity, x = 5, y = 5;
+var monsters = [], velocity, x = 0, y = 5;
 for(var i=0; i<5; i++) {
     velocity = {x: 5, y: 2};
-    monsters.push(new monster(x+= 0,y += 20,velocity,5));
+    monsters.push(new monster(x-= 0,y += 20,velocity,5));
 }
 
 /******* Turrets *********/
 var turrets = [];
 x = 0, y = 0;
 for(var i=0; i<2; i++) {
-    turrets.push(new turret(x+=50,y +=50, 5));
+    turrets.push(new turret(x+=110,y +=50, 5));
 }
 
 function drawMonsters() {
     var interval;
     monsters.forEach(function(monster, index) {
-        interval = (index + 1) * 1000;
-        console.log(now + index > Date.now());
         if(monster.active) {
             monster.draw();
             
@@ -78,7 +78,7 @@ function drawMonsters() {
                     document.location.reload();
                 }
             }
-           
+
             monster.x += monster.vx;        
         }
         
@@ -88,28 +88,26 @@ function drawMonsters() {
 
 function drawTurrets() {
   turrets.forEach(function(turret, index) {
-      if(turret.active) {
-          turret.draw();
-      }
+        turret.draw();
+        anyMonstersInRange(turret);
   });  
 };
 
 function drawLives() {
     ctx.font = "10px serif";
     ctx.fillStyle = "black";
-    ctx.fillText("Lives: " + player.lives, 5, 15);
+    ctx.fillText("Lives: " + player.lives + ", Money: " + player.money, 5, 15);
 };
 
 function draw() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawMonsters();
     drawTurrets();
-
+    
     raf = requestAnimationFrame(draw);
 };
 
 canvas.addEventListener('mouseover', function(e){
-    now = Date.now();
     raf = requestAnimationFrame(draw);
 });
 
@@ -117,10 +115,12 @@ canvas.addEventListener("mouseout",function(e){
     raf = cancelAnimationFrame(raf);
 });
 
-function anyTurretsInRange(monster) {
-    turrets.forEach(function(turret) {
-        if(euclidDistance(monster.x, monster.y, turret.x, turret.y)) {
-            
+function anyMonstersInRange(turret) {
+    monsters.forEach(function(monster) {
+        if(monster.active && euclidDistance(monster.x, monster.y, turret.x, turret.y) < turret.range) {
+            fireTurret(turret, monster);
+            turret.engaged = true;
+            return;
         }
     });
 };
@@ -139,6 +139,33 @@ function outOfBounds(monster) {
     
     return false;
 };
+
+function fireTurret(turret, monster) {
+    monster.lives -= turret.damage;
+    
+    animateShot(turret, monster.x, monster.y);
+
+    if(monster.lives <= 0) {
+        player.money += monster.cost;
+        monster.active = false;
+    }
+};
+
+function animateShot(turret,x,y) {
+    var bulletX, bulletY;
+    var dx = turret.x - x; 
+    var dy = turret.y - y;
+    var distance = dx*dx + dy*dy;
+    var factor = distance / turret.bulletSpeed;
+    var bulletX = dx / factor; 
+    var bulletY = dy / factor;  
+    
+    ctx.beginPath();
+    ctx.arc(bulletX, bulletY, 5, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+};  
 
 function collision(circle1, circle2) {
     var dx = circle1.x - circle2.x;
@@ -160,6 +187,7 @@ function removeMonster(array, monsterIndex) {
     return array;
 };
 
+drawMonsters();
 drawTurrets();
 drawLives();
 
@@ -168,15 +196,15 @@ function rotateTurret(o, event) {
     var deg = getAngle(o, event);
     degreesElement.setHTML(deg);
     o.setStyle('transform', 'rotate(' + deg + 'deg)');
-  }  
-   function animateShot(x,tX,y,tY) {
-    var myrect = mygraphic.addShape({
-        type: "rect",
-        width: 2,
-        height: 2,
-        fill: {
-            color: "#0000ff"
-        },
-        x: tX,
-        y: tY
-    });  	
+};  
+
+function getAngle(o, event) {
+    var dx, dy, tX = o.getX(), tY = o.getY(), mX = event.clientX, mY = event.clientY;
+
+    dx = mX - tX;
+    dy = mY - tY;
+
+    d =  Math.atan2(dy, dx) / Math.PI * 180 + 79;
+
+    return Math.round(d); 
+};
